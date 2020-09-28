06c4d76e028de47ff4cd29758ce5138f0a131a32
Helder Boone <helder.boone@gmail.com>
2020-09-28 00:52:55 -0300

    Remove loan user endpoint

--- src/Lender.API/Controllers/UserController.cs
+++ src/Lender.API/Controllers/UserController.cs
@@ -9,12 +9,6 @@ namespace Lender.API.Controllers
     [Route("api/[controller]")]
     public class UserController : BaseApiController
     {
-        
-
-        [HttpGet("loan")]
-        public async Task<IActionResult> List()
-        {
-            return Ok(await Mediator.Send(new LoanListQuery()));
-        }        
+             
     }
 }
